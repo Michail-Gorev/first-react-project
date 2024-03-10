301af2b54212c5f8698453909b51776ea52dd9fc
Michail-Gorev <90262826+Michail-Gorev@users.noreply.github.com>
2024-03-10 21:12:53 +0300

    Clear status feature added

--- src/SetStatus.js
+++ src/SetStatus.js
@@ -13,13 +13,18 @@ const Status = () => {
     const handleButtonClick = () => {
         setStatusText(inputText);
     };
+    const clearStatus = () => {
+        setStatusText('');
+    };
 
     return (
         <div>
             <h6>{statusText}</h6>
             <input type="text" value={inputText} onChange={handleInputChange} />
             <p></p>
-            <button className={"btn btn-success"} onClick={handleButtonClick}>Установить статус</button>
+            <button id={"setStatus"} className={"btn btn-success"} onClick={handleButtonClick}>Установить статус</button>
+            <p></p>
+            <button id={"clearStatus"} className={"btn btn-danger"} onClick={clearStatus}>Удалить статус</button>
         </div>
     );
 };
