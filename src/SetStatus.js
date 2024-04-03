fcec14d38f58ea11a28041b872ceb6b7a2dd0ebd
Michail-Gorev <90262826+Michail-Gorev@users.noreply.github.com>
2024-04-03 08:31:22 +0300

    useLocalStorage added

--- src/SetStatus.js
+++ src/SetStatus.js
@@ -1,26 +1,30 @@
 import React from 'react'
 import {useState} from "react";
+import { useLocalStorage} from "./hooks/useLocalStorage";
 
 
 const Status = () => {
     const [inputText, setInputText] = useState('');
     const [statusText, setStatusText] = useState('Крутой статус');
+    const [status, setStatus] = useLocalStorage([], 'status');
 
     const handleInputChange = (e) => {
+        setStatus(e.target.value);
         setInputText(e.target.value);
     };
 
     const handleButtonClick = () => {
         setStatusText(inputText);
+        setInputText('');
     };
     const clearStatus = () => {
         setStatusText('');
         setInputText('');
+        setStatus('')
     };
-
     return (
         <div>
-            <h6>{statusText}</h6>
+            <h6>{status}</h6>
             <input type="text" value={inputText} onChange={handleInputChange} />
             <p></p>
             <button id={"setStatus"} className={"btn btn-success"} onClick={handleButtonClick}>Установить статус</button>
